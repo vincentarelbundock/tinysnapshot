7717dad3ebf3470b0b5287388d718c5680200ac6
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-05-18 13:02:26 -0400

    simplify

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,5 @@
-register_tinytest = function() {
-  ns = getNamespace("tinyviztest")
-  expectations = names(ns)[grepl("^expect_", names(ns))]
-  tinytest::register_tinytest_extension("tinyviztest", expectations)
+.onLoad = function(libname, pkgname) {
+    tinytest::register_tinytest_extension(
+        "tinyviztest",
+        c("expect_vdiff"))
 }
-
-.onLoad = function(libpath, pkgname) {
-  backports::import(pkgname)
-  if ("tinytest" %in% loadedNamespaces())
-    register_tinytest()
-  setHook(packageEvent("tinytest", "onLoad"), function(...) register_tinytest(), action = "append")
-}
-
