645c52c021cd761626d2c3ee439a557b529c736c
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-12-29 21:30:30 -0500

    label must be portable

---
+++ R/portable_label.R
@@ -0,0 +1,12 @@
+portable_label <- function(label, warn = TRUE) {
+    regex <- "[^[:alnum:]|_]"
+    out <- gsub(regex, "_", label)
+    if (isTRUE(warn) && grepl(regex, label)) {
+        msg <- 'The `label` must be a "portable" string with only alpha-numeric characters or underscores. Other characters were replaced by underscores:
+        %s 
+        %s'
+        msg <- sprintf(msg, label, out)
+        warning(msg, call. = TRUE)
+    }
+    return(out)
+}
