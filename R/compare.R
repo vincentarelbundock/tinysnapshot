5e50f7c0ccb3dde3b80e77a79cb2c8b657532b66
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-12-29 19:33:46 -0500

    bugfix

--- R/compare.R
+++ R/compare.R
@@ -3,12 +3,13 @@ compare <- function(x, label, tolerance = 0) {
         revdir <- file.path("_tinyviztest_review", label)
         dir.create(revdir, showWarnings = FALSE, recursive = TRUE)
         file.rename(x$new, file.path(revdir, "new.png"))
-        file.rename(x$old, file.path(revdir, "old.png"))
         file.rename(x$diff, file.path(revdir, "diff.png"))
+        file.copy(x$old, file.path(revdir, "old.png"))
         flag <- FALSE
     } else {
         flag <- TRUE
     }
+
     unlink(x$tmp_dir, recursive = TRUE, force = TRUE)
     return(flag)
 }
