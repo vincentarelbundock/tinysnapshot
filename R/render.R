de3be1c552d9c5ffbd50000d33584dff5106a7a4
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-12-29 19:46:45 -0500

    deprecate gdiff

--- R/render.R
+++ R/render.R
@@ -5,31 +5,37 @@ render <- function(x, path, device =  gdiff::pngDevice()) {
     }
 
     # support both types
+    png(path)
     if (inherits(x, "ggplot")) {
-        obj <- function() print(x)
+        print(x)
     } else if (is.function(x)) {
-        obj <- x
+        x()
     } else {
         msg <- "Must be an object of class `ggplot` or a function which prints a plot."
         stop(msg, call. = FALSE)
     }
+    dev.off()
 
+    return(invisible(NULL))
 
-    # `gdiff` requires directory structure
-    randdir <- sample(c(0:9, letters), 20, replace = TRUE)
-    randdir <- paste0("tinyviztest_render_", paste(randdir, collapse = ""))
-    unlink(".gdiffSession")
+    ##### old code works with `gdiff`
 
-    fn <- suppressWarnings(gdiff::gdiffOutput(
-        obj,
-        dir = randdir,
-        device = device,
-        clean = TRUE))
+    # # `gdiff` requires directory structure
+    # randdir <- sample(c(0:9, letters), 20, replace = TRUE)
+    # randdir <- paste0("tinyviztest_render_", paste(randdir, collapse = ""))
 
-    pathdir <- dirname(path)
-    dir.create(pathdir, showWarnings = FALSE, recursive = TRUE)
-    file.rename(fn[1], path)
+    # unlink(".gdiffSession")
 
-    unlink(".gdiffSession")
-    unlink(randdir, recursive = TRUE, force = TRUE)
+    # fn <- suppressWarnings(gdiff::gdiffOutput(
+    #     obj,
+    #     dir = randdir,
+    #     device = device,
+    #     clean = TRUE))
+
+    # pathdir <- dirname(path)
+    # dir.create(pathdir, showWarnings = FALSE, recursive = TRUE)
+    # file.rename(fn[1], path)
+
+    # unlink(".gdiffSession")
+    # unlink(randdir, recursive = TRUE, force = TRUE)
 }
--- trash.R
+++
@@ -1,6 +0,0 @@
-p = function() plot(mtcars$wt, mtcars$mpg)
-
-pkgload::load_all()
-distance_plot(p, "inst/tinytest/_tinyviztest/base.png", metric = "AE")
-
-distance_plot(p, "inst/tinytest/_tinyviztest/base.png", metric = "Fuzz")
