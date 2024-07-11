a16c867ea9209393164c9da8f5ab85fcac73a7c2
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-07-11 07:47:30 +0200

    simplify checks for non alphanumeric labels

--- R/utils.R
+++ R/utils.R
@@ -1,13 +1,7 @@
 snapshot_label <- function(label) {
     regex <- "[^[:alnum:]|_|-]"
-    out <- gsub(regex, "_", label)
-    if (grepl(regex, label)) {
-        msg <- 'The `label` argument must be a "portable" string with only alpha-numeric characters, hyphens, or underscores. Other characters were replaced by underscores:
-        %s 
-        %s'
-        msg <- sprintf(msg, label, out)
-        warning(msg, call. = TRUE)
-    }
+    regexdot <- "[^[:alnum:]_|\\-|\\.]"
+    out <- gsub(regexdot, "_", label, perl = TRUE)
     return(out)
 }
 
