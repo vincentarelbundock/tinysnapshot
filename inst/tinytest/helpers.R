9856ec3e3aa556bc51d62c7b38e4e2609d429f6c
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-07-16 05:55:49 -0400

    skip tests

--- inst/tinytest/helpers.R
+++ inst/tinytest/helpers.R
@@ -1,3 +1,6 @@
 rm(list = ls())
 options(width = 10000)
 options(tinysnapshot_os = c("Linux")) # , "Darwin"))
+
+SKIP = !identical(Sys.getenv("NOT_CRAN"), "true") ||
+  !Sys.info()[["sysname"]] %in% c("Linux")
